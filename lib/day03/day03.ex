613431622bc5161662dc7634453920c0b62cbb33
Rafael Ferreira <rafa@rafasf.com>
2024-12-08 20:26:53 +0100

    Add day 3 part 2

--- lib/day03/day03.ex
+++ lib/day03/day03.ex
@@ -1,28 +1,48 @@
 defmodule Day03 do
   def parse(file_name) do
     File.read!("lib/day03/#{file_name}")
-    |> String.split("\n", trim: true)
+    # |> String.split("\n", trim: true)
   end
 
   def part1(input) do
-    input
-    |> Enum.flat_map(&to_valid_instructions/1)
+    Regex.scan(~r/mul\(\d+,\d+\)/, input)
+    |> Enum.map(&hd/1)
     |> Enum.flat_map(&to_executed_instruction/1)
     |> Enum.sum()
   end
 
-  defp to_valid_instructions(line) do
-    Regex.scan(~r/mul\(\d+,\d+\)/, line)
-    |> Enum.map(&hd/1)
-  end
-
   defp to_executed_instruction(instruction) do
     Regex.scan(~r/(\d+),(\d+)/, instruction)
-    |> Enum.map(&tl/1)
-    |> Enum.map(fn [a, b] -> String.to_integer(a) * String.to_integer(b) end)
+    |> Enum.map(fn [_, a, b] -> String.to_integer(a) * String.to_integer(b) end)
   end
 
   def part2(input) do
-    input
+    valid_instructions_from(input)
+    |> Enum.flat_map(&to_executed_instruction/1)
+    |> Enum.sum()
+  end
+
+  defp valid_instructions_from(line) do
+    Regex.scan(~r/(mul\(\d+,\d+\))|(do\(\))|(don't\(\))/, line)
+    |> Enum.map(&List.last/1)
+    |> Enum.reduce({:do, []}, fn match, {token, acc} ->
+      cond do
+        Regex.match?(~r/don\'t\(\)/, match) ->
+          {:dont, acc}
+
+        Regex.match?(~r/do\(\)/, match) ->
+          {:do, acc}
+
+        token == :do ->
+          {token, acc ++ [match]}
+
+        token == :dont ->
+          {:dont, acc}
+
+        true ->
+          {token, acc}
+      end
+    end)
+    |> elem(1)
   end
 end
--- test/day03_test.exs
+++ test/day03_test.exs
@@ -3,13 +3,17 @@ defmodule Day03Test do
   doctest Day03
 
   test "parses input file" do
-    assert Day03.parse("input_sample.txt") == [
-             "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-           ]
+    assert Day03.parse("input_sample.txt") ==
+             "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))\n"
   end
 
   test "returns sum of valid instructions" do
     input = Day03.parse("input_sample.txt")
     assert Day03.part1(input) == 161
+  end
+
+  test "returns sum of valid instructions considering enable/disable" do
+    input = Day03.parse("input_sample_part_2.txt")
+    assert Day03.part2(input) == 48
   end
 end
